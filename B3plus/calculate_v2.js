b3685399aabaf78273fb1f87c1cb5371d589f893
dementey <dementeyyy@gmail.com>
2018-03-08 23:56:21 +0300

    B3+ v.2

---
+++ B3plus/calculate_v2.js
@@ -0,0 +1,67 @@
+"use strict";
+
+function calc(str){
+    var i=0;
+
+    var currentSymbol = () => str.charAt(i++);
+
+    var additionAndASubtraction = function (){
+        var t1 = multiplicationAndDivision();
+        var t2;
+        while ( (c == "+") || (c == "-")) {
+            var h = c;
+            c =currentSymbol();
+            t2 = multiplicationAndDivision();
+            if (h=="+") t1 = t1 + t2;
+            else t1= t1 - t2;
+        }
+    return Number(t1); 
+    }
+
+    var multiplicationAndDivision = function (){
+        var t1 = calcFragment();
+        var t2;
+        while (c == "*" || (c == "/")){
+            var h = c;
+            c = currentSymbol();
+            t2 = calcFragment();
+            if (h=="*") t1 = t1 * t2;
+            else t1 = t1 / t2;
+        }
+    return Number(t1); 
+    }
+
+    var calcFragment = function (){
+        if ((c >= "1") && (c <= "9")){
+            var h = Number(c);
+            c = currentSymbol();
+            return Number(h); 
+        }
+        else{
+            if (c == "("){
+                c = currentSymbol();
+                var t = additionAndASubtraction();
+                if (c = ")")  {
+                    c = currentSymbol ();
+                    return Number(t);
+                } 
+            }
+        } 
+    }
+
+   
+    var c = currentSymbol();
+    var result = additionAndASubtraction();
+    return result;
+ }
+
+ alert('Результат вычисления равен '+calc(prompt("Введите строку","2*(1+3)")));
+ alert('Результат вычисления равен '+calc(prompt("Введите строку","(1+3)*2")));
+ alert('Результат вычисления равен '+calc(prompt("Введите строку","2+3*4")));
+ alert('Результат вычисления равен '+calc(prompt("Введите строку","3*4+2")));
+
+//B3+
+//Написать функцию-калькулятор вручную введённого выражения (без использования функции eval и динамического описания функции new Function, 
+//если вы знаете о них).
+//Должны работать операции + - * / и скобки, числа должны приниматься целые, дробные (через точку), отрицательные
+//Например, вызываем функцию, передавая ей строку "2*(3+1)", функция возвращает 8.

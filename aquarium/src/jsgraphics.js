243b7ce8b906053fa9591f05941acaea77843366
dementey <dementeyyy@gmail.com>
2018-05-14 16:13:28 +0300

    2018-05-14

--- aquarium/src/jsgraphics.js
+++ aquarium/src/jsgraphics.js
@@ -1,50 +1,45 @@
-/**
- * @author pworman
- */
-
 /* establish window size*/
 
 /*var winw=window.innerWidth
 || document.documentElement.clientWidth
 || document.body.clientWidth;*/
 
-var winw=screen.width
-var winh=screen.height
+var winw = screen.width
+var winh = screen.height
+console.log(winw+'x'+winh );
 
 /*var winh=window.innerHeight
 || document.documentElement.clientHeight
 || document.body.clientHeight;*/
 
-function randomInt (min, max) {
+function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function convert2Hex()
-{
-var numberAsk=parseInt(window.prompt('Enter a number'));
+function convert2Hex() {
+    var numberAsk = parseInt(window.prompt('Enter a number'));
 
-var answer=numberAsk.toString(16);
+    var answer = numberAsk.toString(16);
 
-window.alert(answer)
+    window.alert(answer)
 }
 
 
-function randomColour()
-{return '#'+Math.random().toString(16).substring(4)}
+function randomColour() { return '#' + Math.random().toString(16).substring(4) }
 
-function switchBG()
-{
-	colour='#'+Math.random().toString(16).substring(4);
-	
-	document.body.style.backgroundColor=colour;
-		
+function switchBG() {
+    colour = '#' + Math.random().toString(16).substring(4);
+
+    document.body.style.backgroundColor = colour;
+
 }
-function randomRGB(){
-    var r = function () { return Math.floor(Math.random()*256) };
+function randomRGB() {
+    var r = function () { return Math.floor(Math.random() * 256) };
     return r() + "," + r() + "," + r();
 }
 
-document.getElementById("dimensions").innerHTML=("Window dimensions are: "+w+"x"+h);
+//ocument.getElementById("dimensions").innerHTML = ("Window dimensions are: " + winw + "x" +winh);
 
-document.getElementById("dateline").innerHTML=("The date is "+Date());
+//document.getElementById("dateline").innerHTML = ("The date is " + Date());
+//console.log(winw);
 

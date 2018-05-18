ea10912998c4869673dea15718d0d95a827223fd
dementey <dementeyyy@gmail.com>
2018-05-18 22:54:33 +0300

    2018-05-18

--- aquarium/landingpage.js
+++ aquarium/landingpage.js
@@ -11,7 +11,7 @@ function splashScreen() {
 	document.body.appendChild(logoscreen);
 
 	var logo = document.createElement('div');
-	logo.style.textAlign ='center';
+	logo.style.textAlign = 'center';
 	logoscreen.appendChild(logo);
 	var logo1 = document.createElement('img');
 	logo1.src = "img/logo.svg"
@@ -59,73 +59,64 @@ function splashScreen() {
 /*---------------NETWORK-------------------*/
 
 
-var AjaxHandlerScript="http://fe.it-academy.by/AjaxStringStorage2.php";
+var AjaxHandlerScript = "http://fe.it-academy.by/AjaxStringStorage2.php";
 var UpdatePassword;
 
-function RestoreInfo()
-{
-    $.ajax(
-        {
-            url : AjaxHandlerScript, type : 'POST', cache : false,
-            data : { f : 'READ', n : 'Makarenko_astrojunk' },
-            success : ReadReady, error : ErrorHandler
-        }
-    );
+function RestoreInfo() {
+	$.ajax(
+		{
+			url: AjaxHandlerScript, type: 'POST', cache: false,
+			data: { f: 'READ', n: 'Makarenko_astrojunk' },
+			success: ReadReady, error: ErrorHandler
+		}
+	);
 }
 
-function ReadReady(ResultH)
-{
-    if ( ResultH.error!=undefined )
-        alert(ResultH.error);
-    else if ( ResultH.result!="" )
-    {
-        var InfoH=JSON.parse(ResultH.result);
-        scorecell.innerHTML += InfoH
-    }
+function ReadReady(ResultH) {
+	if (ResultH.error != undefined)
+		alert(ResultH.error);
+	else if (ResultH.result != "") {
+		var InfoH = JSON.parse(ResultH.result);
+		scorecell.innerHTML += InfoH
+	}
 }
 
-function StoreInfo()
-{
-    UpdatePassword=Math.random();
-    $.ajax(
-        {
-            url : AjaxHandlerScript, type : 'POST', cache : false,
-            data : { f : 'LOCKGET', n : 'Haiduk_aquarium', p : UpdatePassword },
-            success : LockGetReady, error : ErrorHandler
-        }
-    );
+function StoreInfo() {
+	UpdatePassword = Math.random();
+	$.ajax(
+		{
+			url: AjaxHandlerScript, type: 'POST', cache: false,
+			data: { f: 'LOCKGET', n: 'Haiduk_aquarium', p: UpdatePassword },
+			success: LockGetReady, error: ErrorHandler
+		}
+	);
 }
 
-function LockGetReady(ResultH)
-{
-    if ( ResultH.error!=undefined )
-        alert(ResultH.error);
-    else
-    {        
-        var InfoH=
-        {
-            name : 'ajname',
-            score : 123
-        };
-        $.ajax(
-            {
-                url : AjaxHandlerScript, type : 'POST', cache : false,
-                data : { f : 'UPDATE', n : 'Haiduk_aquarium, v : JSON.stringify(InfoH), p : UpdatePassword },
-                success : UpdateReady, error : ErrorHandler
-            }
-        );
-    }
+function LockGetReady(ResultH) {
+	if (ResultH.error != undefined)
+		alert(ResultH.error);
+	else {
+		var InfoH =
+			{
+				name: 'ajname',
+				score: 123
+			};
+		$.ajax({
+				url: AjaxHandlerScript, type: 'POST', cache: false,
+				data: {
+					f: 'UPDATE', n: 'Haiduk_aquarium, v: JSON.stringify(InfoH), p: UpdatePassword },
+                success: UpdateReady, error: ErrorHandler
+				}
+        );}
 }
 
-function UpdateReady(ResultH)
-{
-    if ( ResultH.error!=undefined )
-        alert(ResultH.error);
+function UpdateReady(ResultH) {
+	if (ResultH.error != undefined)
+		alert(ResultH.error);
 }
 
-function ErrorHandler(jqXHR,StatusStr,ErrorStr)
-{
-    alert(StatusStr+' '+ErrorStr);
+function ErrorHandler(jqXHR, StatusStr, ErrorStr) {
+	alert(StatusStr + ' ' + ErrorStr);
 }
 
 
